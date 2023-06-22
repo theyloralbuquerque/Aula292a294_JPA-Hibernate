372b979d3647b97394fa278bbd867dfdce1d00d4
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-06-21 22:27:41 -0300

    Searching a record by id

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -9,20 +9,16 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa"); // Um objeto EntityManagerFactory é uma fábrica de EntityManager.
 		EntityManager em = emf.createEntityManager(); // EUm objeto ntityManager gerencia as operações de persistência em entidades. 
 		
-		em.getTransaction().begin(); // Inicia uma transação no contexto de persistência do EntityManager no Java. 
-		em.persist(p1); // .persist() salva uma entidade em um banco de dados.
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit(); // Confirma uma transação no contexto de persistência do EntityManager no Java. 
-		
+		Pessoa p = em.find(Pessoa.class, 2); // .find() chamado a partir de um objeto EntityManager permite obter uma linha do BD buscando pelo id(PK).
+											 // O primeiro argumento é a classe (entidade) que representa a tabela.
+											 // O segundo argumento é o id da linha que eu desejo recuperar.		
+		System.out.println(p);
 		System.out.println("Pronto!");	
+		emf.close();
+		em.close();
 	}
 }
