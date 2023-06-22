753e7428550aa301b40d46047869c8b73a04b376
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-06-21 22:39:59 -0300

    Removing a record from DB

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -13,10 +13,11 @@ public class Programa {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa"); // Um objeto EntityManagerFactory é uma fábrica de EntityManager.
 		EntityManager em = emf.createEntityManager(); // EUm objeto ntityManager gerencia as operações de persistência em entidades. 
 		
-		Pessoa p = em.find(Pessoa.class, 2); // .find() chamado a partir de um objeto EntityManager permite obter uma linha do BD buscando pelo id(PK).
-											 // O primeiro argumento é a classe (entidade) que representa a tabela.
-											 // O segundo argumento é o id da linha que eu desejo recuperar.		
-		System.out.println(p);
+		Pessoa p = em.find(Pessoa.class, 2); // Recuperei um registro para que EntityManager possa monitorá-lo.
+		em.getTransaction().begin();
+		em.remove(p);                        // .remove() remove um registro do BD, desde que ele esteja monitorado pelo EntityManager.
+		em.getTransaction().commit();
+		
 		System.out.println("Pronto!");	
 		emf.close();
 		em.close();
